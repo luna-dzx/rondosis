245ca7c6fc45d5b56c3083b73ea655666005033f
luna-dzx <luna.l.dalzell@gmail.com>
2023-06-23 23:16:46 +0100

    Project Setup

---
+++ src/calculations.rs
@@ -0,0 +1,8 @@
+use bevy_ecs::prelude::*;
+
+#[derive(Component)]
+struct Position { 
+    x: f32, 
+    y: f32 
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod tests;
+
+mod rendering;
+mod calculations;
+
+use crate::{
+    rendering::*,
+    calculations::*
+};
+
+use notan::app::Event;
+use notan::draw::*;
+use notan::prelude::*;
+
+#[notan_main]
+fn main() -> Result<(), String>{
+    notan::init_with(State::new)
+        .add_config(DrawConfig)
+        .draw(draw)
+        .build()
 }
