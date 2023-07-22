c1f0724007693e857eb2a6ec3c63d98bd5956b5a
dylan <dylan_hanley@protonmail.com>
2023-07-22 18:32:07 +0100

    rendering using ecs, clean and cool

--- src/calculations/mod.rs
+++
@@ -1,7 +0,0 @@
-use bevy_ecs::prelude::*;
-use nalgebra::{
-    Vector2,
-};
-
-mod basics;
-
--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,21 @@ mod calculations;
 
 #[notan_main]
 fn main() -> Result<(), String>{
-    notan::init_with(State::new)
+    notan::init_with(SimState::new)
         .add_config(DrawConfig)
+        .update(update)
         .draw(draw)
         .build()
+}
+
+pub fn update(app: &mut App, state: &mut SimState) {
+    state.update_schedule.run(&mut state.world);
+}
+
+pub fn draw(app: &mut App, gfx: &mut Graphics, state: &mut SimState) {
+    state.draw_schedule.run(&mut state.world);
+
+    let notan_draw = state.world.get_resource::<NotanDraw>().unwrap();
+
+    gfx.render(&notan_draw.0);
 }
